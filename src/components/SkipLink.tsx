0ba6d8257164cf995e8fc3f94034cc4d491d39f1
Vilmos Hosztafi <hosztafi@vilmos.co.uk>
2025-09-14 12:45:45 +0100

    feat(skiplink): increase z-index for focus visibility

--- src/components/SkipLink.tsx
+++ src/components/SkipLink.tsx
@@ -11,7 +11,7 @@ export default function SkipLink({ targetId, label, className }: SkipLinkProps) 
     <a
       href={`#${targetId}`}
       className={twMerge(
-        'font-weight-300 absolute left-[-9999px] top-0 box-border bg-white p-4.5 text-left text-lg font-normal leading-[27px] text-[#03071c] underline opacity-0 focus:fixed focus:left-6 focus:top-0 focus:opacity-100 focus:shadow-inset-1 focus-visible:outline-none',
+        'font-weight-300 absolute left-[-9999px] top-0 z-50 box-border bg-white p-4.5 text-left text-lg font-normal leading-[27px] text-[#03071c] underline opacity-0 focus:fixed focus:left-6 focus:top-0 focus:opacity-100 focus:shadow-inset-1 focus-visible:outline-none',
         className
       )}
     >
