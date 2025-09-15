5a2f78d7455decc5c3401661666bee0e7695dcc1
Vilmos Hosztafi <hosztafi@vilmos.co.uk>
2025-09-15 10:04:37 +0100

    chore(config): add custom screen breakpoints to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,6 +35,14 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: '480px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1440px',
+      },
       colors: {
         tfl: {
           ink: '#03071c',
