79c1e0fa74d49cc14526fac15850fd6b82282a9a
Vilmos Hosztafi <hosztafi@vilmos.co.uk>
2025-09-15 01:26:49 +0100

    feat(App): integrate LineStatusGrid and enhance filtering logic with tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import './styles/global.css';
 
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import Layout from './components/Layout';
-import Tabs from './components/Tabs.tsx';
+import LineStatusGrid from './components/LineStatusGrid.tsx';
+import Tabs, { type TabItem } from './components/Tabs.tsx';
+import { useTubeStatuses } from './hooks/useTubeStatuses.ts';
+import { mapToVM } from './lib/utils.ts';
 
 function App() {
-  const [, setSelectedTab] = useState('all');
+  const [selectedTab, setSelectedTab] = useState('all');
+  const { data } = useTubeStatuses();
 
-  const tabs = [
+  const items = useMemo(() => mapToVM(data ?? []), [data]);
+
+  const filtered = useMemo(
+    () =>
+      items?.filter((l) => {
+        switch (selectedTab) {
+          case 'all':
+            return true;
+          case 'active':
+            return !l.canExpand;
+          case 'paused':
+            return l.canExpand;
+          default:
+            return true;
+        }
+      }),
+    [items, selectedTab]
+  );
+
+  const tabs: TabItem[] = [
     { id: 'all', label: 'All' },
     { id: 'active', label: 'Active' },
     { id: 'paused', label: 'Paused' },
@@ -22,7 +45,8 @@ function App() {
             Status
           </h1>
 
-          <Tabs items={tabs} onValueChange={(id) => setSelectedTab(id)} bordered={true} />
+          <Tabs items={tabs} onValueChange={(id) => setSelectedTab(id)} bordered={false} />
+          <LineStatusGrid items={filtered || []} />
         </section>
       </Layout>
     </>
